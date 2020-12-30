e30b533a790541f9bc7f87bec2d8f9b333fdbd19
Florian Bernhardt <fbex@posteo.de>
2020-12-30 23:34:20 +0100

    Add install on various OSs job to github-actions

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import codecs
+import os
 import pathlib
+import re
 
 from setuptools import find_packages, setup
 
@@ -8,9 +11,43 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 install_requires = ['requests']
 tests_require = ['pytest', 'pytest-mock', 'responses']
 
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+NAME = "renamedia"
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    META_PATH
+except NameError:
+    META_PATH = os.path.join(HERE, "src/rename", "__init__.py")
+finally:
+    META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta), META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
 setup(
     name="renamedia",
-    version="0.0.1",
+    version=find_meta("version"),
     description='Rename media files inside a given directory',
     long_description=long_description,
     long_description_content_type='text/markdown',
